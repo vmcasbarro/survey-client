df800aac64a14d660b086140c3ddcbc3b3a197e4
doobin <doobin42@gmail.com>
2018-11-06 12:14:08 -0500

    Feature: Auth UI
    Sign Up and Sign In forms clear if signUpSuccess and signInSuccess
    Sign Up and Sign In forms hide upon signInSuccess

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,31 +3,36 @@
 const store = require('../store.js')
 
 const signUpSuccess = () => {
-
+  $('#sign-up-form').trigger('reset')
 }
 
 const signUpFailure = () => {
-
+  $('#sign-up-form').trigger('reset')
 }
 
 const signInSuccess = (response) => {
   store.user = response.user
+  $('#sign-in-form').trigger('reset')
+  $('#sign-up-form').addClass('hidden')
+  $('#sign-in-form').addClass('hidden')
 }
 
 const signInFailure = () => {
-
+  $('#sign-in-form').trigger('reset')
 }
 
 const passwordChangeSuccess = () => {
-
+  $('#change-password-form').trigger('reset')
+  $('#change-password-form').addClass('hidden')
 }
 
 const passwordChangeFailure = () => {
-
+  $('#change-password-form').trigger('reset')
 }
 
 const signOutSuccess = () => {
-
+  $('#sign-up-form').removeClass('hidden')
+  $('#sign-in-form').removeClass('hidden')
 }
 
 const signOutFailure = () => {
