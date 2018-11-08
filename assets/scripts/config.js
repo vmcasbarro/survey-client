508c1ecc77da4f84616e24165e6ebd0b54a85a9e
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-11-08 10:46:57 -0500

    eliminate trailing `/` on deployment url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://blooming-springs-16841.herokuapp.com/',
+  production: 'https://blooming-springs-16841.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
